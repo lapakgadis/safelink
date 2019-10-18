053dfd7d317cf64d330a880373c1a0aea44be52a
lapakgadis <51421741+lapakgadis@users.noreply.github.com>
2019-10-18 20:38:57 +0700

    Add files via upload

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="overcoming-road-trauma-in-united-arab.html"} 
 if(x=="2"){xxx="why-my-bankruptcy-attorney-is-better.html"} 
 if(x=="3"){xxx="how-to-become-innovative-leader-skills.html"} 
-if(x=="4"){xxx="#"}a_to_ve[i].href="https://sekisah27.blogspot.com/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="develop-effective-confidence-and.html"}a_to_ve[i].href="https://sekisah27.blogspot.com/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
